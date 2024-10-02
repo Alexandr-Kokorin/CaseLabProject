e929bca3ae2d742992f88ca378fa3ae65f6af56c
Alexandr-Kokorin <105930009+Alexandr-Kokorin@users.noreply.github.com>
2024-10-03 02:58:07 +0300

    test

--- project/src/test/java/caselab/service/ServiceTest.java
+++ project/src/test/java/caselab/service/ServiceTest.java
@@ -1,7 +1,14 @@
 package caselab.service;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 public class ServiceTest {
+
+    @Test
+    void getTest() {
+        assertThat(1).isEqualTo(1);
+    }
 }
