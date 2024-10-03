20c8c28a2101f6a2e0697005b8167b3f0e2653cf
Alexandr-Kokorin <105930009+Alexandr-Kokorin@users.noreply.github.com>
2024-10-03 17:20:52 +0300

    add readme, liquebase and fix

---
+++ migrations/sql/attribute.sql
@@ -0,0 +1,8 @@
+create table attribute
+(
+    id             bigint         not null,
+    name           text           not null,
+    type           text           not null,
+
+    primary key (id)
+)
---
+++ migrations/sql/document.sql
@@ -0,0 +1,7 @@
+create table document
+(
+    id                     bigint           not null,
+    document_type_id       bigint           not null,
+
+    primary key (id)
+)
