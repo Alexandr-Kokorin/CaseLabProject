ae718f2ed70a9d9cc284ba0833777feeb55d1656
hottabych04 <hottabych04@gmail.com>
2024-10-03 19:59:01 +0300

    Edit liquibase migration

---
+++ migrations/db/changelog/db.changelog-1.0.sql
@@ -0,0 +1,62 @@
+--liquibase formatted sql
+
+--changeset hottabych04:1
+CREATE TABLE IF NOT EXISTS application_user
+(
+    id                  BIGINT        NOT NULL,
+    login               TEXT          NOT NULL,
+    display_name        TEXT          NOT NULL,
+    hashed_password     TEXT          NOT NULL,
+
+    PRIMARY KEY (id)
+)
+
+--changeset hottabych04:2
+CREATE TABLE IF NOT EXISTS attribute
+(
+    id             BIGINT         NOT NULL,
+    name           TEXT           NOT NULL,
+    type           TEXT           NOT NULL,
+
+    PRIMARY KEY (id)
+)
+
+--changeset hottabych04:3
+CREATE TABLE IF NOT EXISTS document_type
+(
+    id              BIGINT        NOT NULL,
+    name            TEXT          NOT NULL,
+
+    PRIMARY KEY (id)
+)
+
+--changeset hottabych04:4
+CREATE TABLE IF NOT EXISTS document
+(
+    id                     BIGINT           NOT NULL,
+    document_type_id       BIGINT           NOT NULL,
+
+    PRIMARY KEY (id)
+)
+
+--changeset hottabych04:5
+CREATE TABLE IF NOT EXISTS user_to_document
+(
+    document_id        BIGINT          NOT NULL REFERENCES document(id) ON DELETE CASCADE,
+    user_id            BIGINT          NOT NULL REFERENCES application_user(id) ON DELETE CASCADE
+)
+
+--changeset hottabych04:6
+CREATE TABLE IF NOT EXISTS document_attribute_value
+(
+    document_id         BIGINT          NOT NULL REFERENCES document(id) ON DELETE CASCADE,
+    attribute_id        BIGINT          NOT NULL REFERENCES attribute(id) ON DELETE CASCADE,
+    app_value           TEXT            NOT NULL
+)
+
+--changeset hottabych04:7
+CREATE TABLE IF NOT EXISTS document_type_to_attribute
+(
+    document_type_id        BIGINT          NOT NULL REFERENCES document_type(id) ON DELETE CASCADE,
+    attribute_id            BIGINT          NOT NULL REFERENCES attribute(id) ON DELETE CASCADE
+)
--- migrations/sql/document_type.sql
+++
@@ -1,7 +0,0 @@
-create table document_type
-(
-    id              bigint        not null,
-    name            text          not null,
-
-    primary key (id)
-)
