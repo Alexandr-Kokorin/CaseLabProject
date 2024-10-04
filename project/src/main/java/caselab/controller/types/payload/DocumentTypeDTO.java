92930373abcc01c9537a62aa4fb9385e5d7f779f
mudan <myudanov223@gmail.com>
2024-10-04 16:29:21 +0300

    Добавлен DTO для типов документов, контроллер для типов документов перенесён в отдельный пакет

---
+++ project/src/main/java/caselab/controller/types/payload/DocumentTypeDTO.java
@@ -0,0 +1,14 @@
+package caselab.controller.types.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class DocumentTypeDTO {
+    private String name;
+}
