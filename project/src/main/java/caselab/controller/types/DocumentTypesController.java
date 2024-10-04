2e17ecfc0c94a36cf2e0ff166be9214d54a7a0f6
mudan <myudanov223@gmail.com>
2024-10-04 18:31:53 +0300

    Добавлен метод GET для получения типа документа по id

--- project/src/main/java/caselab/controller/types/DocumentTypesController.java
+++ project/src/main/java/caselab/controller/types/DocumentTypesController.java
@@ -4,6 +4,8 @@ import caselab.controller.types.payload.DocumentTypeDTO;
 import caselab.service.DocumentTypesService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,9 +18,15 @@ public class DocumentTypesController {
 
     private final DocumentTypesService documentTypesService;
 
-    @Operation(summary = "Добавить")
+    @Operation(summary = "Добавить тип документа")
     @PostMapping
     public DocumentTypeDTO createDocumentType(@RequestBody DocumentTypeDTO documentTypeDTO) {
         return documentTypesService.createDocumentType(documentTypeDTO);
+    }
+
+    @Operation(summary = "Получить тип документа по id")
+    @GetMapping("/{id}")
+    public DocumentTypeDTO findDocumentTypeById(@PathVariable Long id) {
+        return documentTypesService.findDocumentTypeById(id);
     }
 }
