c22abfbe54d941c66011a957f655175a45b72dae
PAZDerev <zdereff.pasha@ya.ru>
2024-10-06 02:27:26 +0500

    test (Document) : Unit и интеграционные тесты для CRUD Document

---
+++ project/src/test/java/caselab/controller/DocumentControllerTest.java
@@ -0,0 +1,124 @@
+package caselab.controller;
+
+import caselab.controller.document.payload.DocumentAttributeValueDTO;
+import caselab.controller.document.payload.DocumentDTO;
+import caselab.domain.entity.exception.ResourceNotFoundException;
+import caselab.service.document.DocumentService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.Arrays;
+import java.util.Collections;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class DocumentControllerTest extends BaseControllerIT {
+
+    private final String DOCUMENT_URI = "/api/v1/documents";
+
+    @MockBean
+    private DocumentService documentService;
+
+    @MockBean
+    private SecurityFilterChain securityFilterChain;
+
+    private DocumentDTO documentDTO;
+
+    @BeforeEach
+    public void setup() {
+        documentDTO = new DocumentDTO();
+        documentDTO.setId(1L);
+        documentDTO.setDocumentTypeId(1L);
+        documentDTO.setApplicationUserIds(Arrays.asList(1L, 2L));
+        DocumentAttributeValueDTO attributeValueDTO = new DocumentAttributeValueDTO();
+        attributeValueDTO.setId(1L);
+        attributeValueDTO.setValue("Test Value");
+        documentDTO.setAttributeValues(Collections.singletonList(attributeValueDTO));
+    }
+
+    @Tag("Create")
+    @DisplayName("Should create document")
+    @Test
+    public void testCreateDocument() throws Exception {
+        // Arrange
+        when(documentService.createDocument(any(DocumentDTO.class))).thenReturn(documentDTO);
+
+        // Act & Assert
+        mockMvc.perform(post(DOCUMENT_URI)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(documentDTO)))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.id").value(documentDTO.getId()))
+            .andExpect(jsonPath("$.documentTypeId").value(documentDTO.getDocumentTypeId()))
+            .andExpect(jsonPath("$.applicationUserIds[0]").value(1))
+            .andExpect(jsonPath("$.attributeValues[0].id").value(1))
+            .andExpect(jsonPath("$.attributeValues[0].value").value("Test Value"));
+    }
+
+    @Tag("GetById")
+    @DisplayName("Should return document for searching existing document")
+    @Test
+    public void testGetDocumentById() throws Exception {
+        // Arrange
+        when(documentService.getDocumentById(1L)).thenReturn(documentDTO);
+
+        // Act & Assert
+        mockMvc.perform(get(DOCUMENT_URI + "/1")
+                .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.id").value(documentDTO.getId()))
+            .andExpect(jsonPath("$.documentTypeId").value(documentDTO.getDocumentTypeId()));
+    }
+
+    @Tag("GetById")
+    @DisplayName("Should return NOT FOUND for searching not existing document")
+    @Test
+    public void testGetDocumentById_NotFound() throws Exception {
+        // Arrange
+        when(documentService.getDocumentById(1L)).thenThrow(new ResourceNotFoundException("Документ не найден"));
+
+        // Act & Assert
+        mockMvc.perform(get(DOCUMENT_URI + "/1")
+                .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isNotFound());
+    }
+
+    @Tag("Update")
+    @DisplayName("Should update existing document by id")
+    @Test
+    public void testUpdateDocument() throws Exception {
+        // Arrange
+        when(documentService.updateDocument(eq(1L), any(DocumentDTO.class))).thenReturn(documentDTO);
+
+        // Act & Assert
+        mockMvc.perform(put(DOCUMENT_URI + "/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(documentDTO)))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.id").value(documentDTO.getId()))
+            .andExpect(jsonPath("$.documentTypeId").value(documentDTO.getDocumentTypeId()));
+    }
+
+    @Tag("Delete")
+    @DisplayName("Should delete existing document by id")
+    @Test
+    public void testDeleteDocument() throws Exception {
+        // Arrange
+        doNothing().when(documentService).deleteDocument(1L);
+
+        // Act & Assert
+        mockMvc.perform(delete(DOCUMENT_URI + "/1"))
+            .andExpect(status().isNoContent());
+    }
+
+}
---
+++ project/src/test/java/caselab/service/DocumentServiceTest.java
@@ -0,0 +1,201 @@
+package caselab.service;
+
+import caselab.controller.document.payload.DocumentAttributeValueDTO;
+import caselab.controller.document.payload.DocumentDTO;
+import caselab.domain.IntegrationTest;
+import caselab.domain.entity.ApplicationUser;
+import caselab.domain.entity.Attribute;
+import caselab.domain.entity.DocumentType;
+import caselab.domain.entity.enums.Role;
+import caselab.domain.entity.exception.ResourceNotFoundException;
+import caselab.domain.repository.ApplicationUserRepository;
+import caselab.domain.repository.AttributeRepository;
+import caselab.domain.repository.DocumentRepository;
+import caselab.domain.repository.DocumentTypesRepository;
+import caselab.service.document.DocumentService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+@Transactional
+@Rollback
+public class DocumentServiceTest extends IntegrationTest {
+
+    @Autowired
+    private DocumentService documentService;
+
+    @Autowired
+    private DocumentRepository documentRepository;
+
+    @Autowired
+    private DocumentTypesRepository documentTypeRepository;
+
+    @Autowired
+    private ApplicationUserRepository applicationUserRepository;
+
+    @Autowired
+    private AttributeRepository attributeRepository;
+
+    private Long documentTypeId;
+    private Long user1Id;
+    private Long user2Id;
+    private Long attributeId;
+
+    @BeforeEach
+    public void setUp() {
+        DocumentType documentType = new DocumentType();
+        documentType.setName("Test Document Type");
+        documentType = documentTypeRepository.save(documentType);
+        documentTypeId = documentType.getId();
+
+        ApplicationUser user1 = ApplicationUser.builder()
+            .login("Test login 1 ")
+            .displayName("Test display name 1")
+            .role(Role.USER)
+            .hashedPassword("abc")
+            .build();
+        ApplicationUser user2 = ApplicationUser.builder()
+            .login("Test login 2 ")
+            .displayName("Test display name 2")
+            .role(Role.USER)
+            .hashedPassword("abc")
+            .build();
+        user1 = applicationUserRepository.save(user1);
+        user1Id = user1.getId();
+        user2 = applicationUserRepository.save(user2);
+        user2Id = user2.getId();
+        Attribute attribute = new Attribute();
+        attribute.setName("Test attribute 1");
+        attribute.setType("String");
+        attribute.setDocumentTypes(List.of(documentType));
+        attribute = attributeRepository.save(attribute);
+        attributeId = attribute.getId();
+
+    }
+
+    @DisplayName("Should create document")
+    @Test
+    public void testCreateDocument() {
+        // Arrange
+        DocumentDTO documentDTO = new DocumentDTO();
+        documentDTO.setDocumentTypeId(documentTypeId);
+        documentDTO.setApplicationUserIds(Arrays.asList(user1Id, user2Id));
+        DocumentAttributeValueDTO attributeValueDTO = new DocumentAttributeValueDTO();
+        attributeValueDTO.setId(attributeId);
+        attributeValueDTO.setValue("Test Value");
+        documentDTO.setAttributeValues(Collections.singletonList(attributeValueDTO));
+
+        // Act
+        DocumentDTO result = documentService.createDocument(documentDTO);
+
+        // Assert
+        assertNotNull(result);
+        assertNotNull(result.getId());
+        assertEquals(documentTypeId, result.getDocumentTypeId());
+        assertEquals(Arrays.asList(user1Id, user2Id), result.getApplicationUserIds());
+        assertEquals(attributeId, result.getAttributeValues().get(0).getId());
+        assertEquals("Test Value", result.getAttributeValues().get(0).getValue());
+    }
+
+    @DisplayName("Should update document type for document")
+    @Test
+    public void testUpdateDocument() {
+        // Arrange
+        DocumentDTO documentDTO = new DocumentDTO();
+        documentDTO.setDocumentTypeId(documentTypeId);
+        documentDTO.setApplicationUserIds(Arrays.asList(user1Id, user2Id));
+        DocumentAttributeValueDTO attributeValueDTO = new DocumentAttributeValueDTO();
+        attributeValueDTO.setId(attributeId);
+        attributeValueDTO.setValue("Test Value");
+        documentDTO.setAttributeValues(Collections.singletonList(attributeValueDTO));
+
+        // Act
+        DocumentDTO result = documentService.createDocument(documentDTO);
+        Long id = result.getId();
+
+        DocumentType newDocumentType = new DocumentType();
+        newDocumentType.setName("Test Document Type 2");
+        newDocumentType = documentTypeRepository.save(newDocumentType);
+        Long updatedDocumentTypeId = newDocumentType.getId();
+
+        DocumentDTO updatingDocumentDTO = new DocumentDTO();
+        updatingDocumentDTO.setDocumentTypeId(updatedDocumentTypeId);
+        updatingDocumentDTO.setApplicationUserIds(result.getApplicationUserIds());
+        updatingDocumentDTO.setAttributeValues(result.getAttributeValues());
+        updatingDocumentDTO.setId(result.getId());
+        updatingDocumentDTO = documentService.updateDocument(id, updatingDocumentDTO);
+
+        // Assert
+        assertNotNull(updatingDocumentDTO);
+        assertEquals(id, updatingDocumentDTO.getId());
+        assertEquals(updatedDocumentTypeId, updatingDocumentDTO.getDocumentTypeId());
+        assertEquals(Arrays.asList(user1Id, user2Id), updatingDocumentDTO.getApplicationUserIds());
+        assertEquals(attributeId, updatingDocumentDTO.getAttributeValues().get(0).getId());
+        assertEquals("Test Value", updatingDocumentDTO.getAttributeValues().get(0).getValue());
+    }
+
+    @DisplayName("Should delete document")
+    @Test
+    public void testDeleteDocument() {
+        // Arrange
+        DocumentDTO documentDTO = new DocumentDTO();
+        documentDTO.setDocumentTypeId(documentTypeId);
+        documentDTO.setApplicationUserIds(Arrays.asList(user1Id, user2Id));
+        DocumentAttributeValueDTO attributeValueDTO = new DocumentAttributeValueDTO();
+        attributeValueDTO.setId(attributeId);
+        attributeValueDTO.setValue("Test Value");
+        documentDTO.setAttributeValues(Collections.singletonList(attributeValueDTO));
+        DocumentDTO result = documentService.createDocument(documentDTO);
+        Long id = result.getId();
+        // Act
+        documentService.deleteDocument(id);
+
+        // Assert
+        assertThrows(ResourceNotFoundException.class, () -> documentService.getDocumentById(id));
+    }
+
+    @DisplayName("Should not found document")
+    @Test
+    public void testDeleteDocumentNotFound() {
+        // Act & Assert
+        assertThrows(ResourceNotFoundException.class, () -> documentService.deleteDocument(1L));
+    }
+
+    @DisplayName("Should return document")
+    @Test
+    public void testGetDocumentById() {
+        // Arrange
+        DocumentDTO documentDTO = new DocumentDTO();
+        documentDTO.setDocumentTypeId(documentTypeId);
+        documentDTO.setApplicationUserIds(Arrays.asList(user1Id, user2Id));
+        DocumentAttributeValueDTO attributeValueDTO = new DocumentAttributeValueDTO();
+        attributeValueDTO.setId(attributeId);
+        attributeValueDTO.setValue("Test Value");
+        documentDTO.setAttributeValues(Collections.singletonList(attributeValueDTO));
+        DocumentDTO result = documentService.createDocument(documentDTO);
+
+        // Act
+        DocumentDTO findById = documentService.getDocumentById(result.getId());
+
+        // Assert
+        assertNotNull(result);
+        assertEquals(result.getId(), findById.getId());
+        assertEquals(documentTypeId, findById.getDocumentTypeId());
+        assertEquals(Arrays.asList(user1Id, user2Id), findById.getApplicationUserIds());
+        assertEquals(attributeId, findById.getAttributeValues().get(0).getId());
+        assertEquals("Test Value", findById.getAttributeValues().get(0).getValue());
+    }
+
+}
