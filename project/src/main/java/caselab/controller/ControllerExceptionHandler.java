ed84a13e36aad66964c862c53418889c84a5a96d
PAZDerev <zdereff.pasha@ya.ru>
2024-10-05 22:16:33 +0500

    feat (Document) : Реализация CRUD операций для класса Document  - Созданы контроллер, сервис, репозиторий для Document  - Создан класс ApiError для возвращения унифицированной информации об ошибке  - Создано исключение ResourceNotFoundException для 404 кода  - Подправлены типы данных в БД

--- project/src/main/java/caselab/controller/ControllerExceptionHandler.java
+++ project/src/main/java/caselab/controller/ControllerExceptionHandler.java
@@ -3,6 +3,8 @@ package caselab.controller;
 import caselab.domain.entity.exception.ApiError;
 import caselab.domain.entity.exception.ResourceNotFoundException;
 import java.util.NoSuchElementException;
+import caselab.domain.entity.exception.ApiError;
+import caselab.domain.entity.exception.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -40,5 +42,23 @@ public class ControllerExceptionHandler {
     @ExceptionHandler(BadCredentialsException.class)
     public ResponseEntity<?> unauthorizedException(BadCredentialsException e) {
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
+    }
+
+    @ExceptionHandler(HttpClientErrorException.NotFound.class)
+    public ResponseEntity<?> notFoundException(HttpClientErrorException.NotFound e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+            .body(e.getMessage());
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ApiError> handleResourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
+        ApiError error = new ApiError();
+        error.setType("about:blank");
+        error.setTitle("Ресурс не найден");
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setDetail(ex.getMessage());
+        error.setInstance(request.getDescription(false));
+
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 }
