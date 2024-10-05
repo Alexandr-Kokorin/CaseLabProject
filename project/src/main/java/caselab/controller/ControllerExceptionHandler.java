9df2eb8ab6933776dbe4344e4ae40670c225637f
Alexandr-Kokorin <105930009+Alexandr-Kokorin@users.noreply.github.com>
2024-10-05 23:29:18 +0300

    Добавлены тесты к JWT

--- project/src/main/java/caselab/controller/ControllerExceptionHandler.java
+++ project/src/main/java/caselab/controller/ControllerExceptionHandler.java
@@ -2,15 +2,16 @@ package caselab.controller;
 
 import caselab.domain.entity.exception.ApiError;
 import caselab.domain.entity.exception.ResourceNotFoundException;
+import java.util.NoSuchElementException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.context.request.WebRequest;
 
-@SuppressWarnings("MagicNumber")
-@RestControllerAdvice(basePackages = {"caselab.controller"})
+@RestControllerAdvice
 public class ControllerExceptionHandler {
 
     @ExceptionHandler(HttpClientErrorException.NotFound.class)
@@ -29,5 +30,15 @@ public class ControllerExceptionHandler {
         error.setInstance(request.getDescription(false));
 
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<?> notFoundException(NoSuchElementException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<?> notFoundException(BadCredentialsException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
     }
 }
---
+++ project/src/test/java/caselab/controller/AuthenticationControllerTest.java
@@ -0,0 +1,88 @@
+package caselab.controller;
+
+import caselab.controller.secutiry.payload.AuthenticationRequest;
+import caselab.controller.secutiry.payload.AuthenticationResponse;
+import caselab.controller.secutiry.payload.RegisterRequest;
+import caselab.service.secutiry.AuthenticationService;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.web.SecurityFilterChain;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class AuthenticationControllerTest extends BaseControllerMockTest {
+
+    private final String AUTH_URI = "/api/v1/auth";
+    @MockBean
+    private AuthenticationService authenticationService;
+    @MockBean
+    private SecurityFilterChain securityFilterChain;
+
+    @Nested
+    @Tag("Register")
+    @DisplayName("Register user")
+    class RegisterTest {
+
+        @SneakyThrows
+        @Test
+        @DisplayName("Should register user with valid payload")
+        public void register_success() {
+            var payload = new RegisterRequest("login", "displayName", "password");
+            var response = AuthenticationResponse.builder().token("token").build();
+
+            when(authenticationService.register(payload)).thenReturn(response);
+
+            var mvcResponse = mockMvc.perform(post(AUTH_URI + "/register")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(payload)))
+                .andExpectAll(
+                    status().is2xxSuccessful(),
+                    content().contentType(MediaType.APPLICATION_JSON)
+                )
+                .andReturn()
+                .getResponse();
+
+            var actualResponse = objectMapper.readValue(mvcResponse.getContentAsString(), AuthenticationResponse.class);
+
+            assertEquals(actualResponse, response);
+        }
+    }
+
+    @Nested
+    @Tag("Authenticate")
+    @DisplayName("Authenticate user")
+    class AuthenticateTest {
+
+        @SneakyThrows
+        @Test
+        @DisplayName("Should authenticate user with valid payload")
+        public void authenticate_success() {
+            var payload = AuthenticationRequest.builder().login("login").password("password").build();
+            var response = AuthenticationResponse.builder().token("token").build();
+
+            when(authenticationService.authenticate(payload)).thenReturn(response);
+
+            var mvcResponse = mockMvc.perform(post(AUTH_URI + "/authenticate")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(payload)))
+                .andExpectAll(
+                    status().is2xxSuccessful(),
+                    content().contentType(MediaType.APPLICATION_JSON)
+                )
+                .andReturn()
+                .getResponse();
+
+            var actualResponse = objectMapper.readValue(mvcResponse.getContentAsString(), AuthenticationResponse.class);
+
+            assertEquals(actualResponse, response);
+        }
+    }
+}
