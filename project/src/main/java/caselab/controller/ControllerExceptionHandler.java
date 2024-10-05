4e19bf58af2acf945bd4a374531f0af3266ac024
PAZDerev <zdereff.pasha@ya.ru>
2024-10-06 02:54:00 +0500

    fix conflicts

--- project/src/main/java/caselab/controller/ControllerExceptionHandler.java
+++ project/src/main/java/caselab/controller/ControllerExceptionHandler.java
@@ -3,8 +3,6 @@ package caselab.controller;
 import caselab.domain.entity.exception.ApiError;
 import caselab.domain.entity.exception.ResourceNotFoundException;
 import java.util.NoSuchElementException;
-import caselab.domain.entity.exception.ApiError;
-import caselab.domain.entity.exception.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -15,24 +13,6 @@ import org.springframework.web.context.request.WebRequest;
 
 @RestControllerAdvice
 public class ControllerExceptionHandler {
-
-    @ExceptionHandler(HttpClientErrorException.NotFound.class)
-    public ResponseEntity<?> notFoundException(HttpClientErrorException.NotFound e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(e.getMessage());
-    }
-
-    @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<ApiError> handleResourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
-        ApiError error = new ApiError();
-        error.setType("about:blank");
-        error.setTitle("Ресурс не найден");
-        error.setStatus(HttpStatus.NOT_FOUND.value());
-        error.setDetail(ex.getMessage());
-        error.setInstance(request.getDescription(false));
-
-        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-    }
 
     @ExceptionHandler(NoSuchElementException.class)
     public ResponseEntity<?> notFoundException(NoSuchElementException e) {
