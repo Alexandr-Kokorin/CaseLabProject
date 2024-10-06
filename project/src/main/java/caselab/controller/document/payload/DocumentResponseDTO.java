7620d004fd62386a7ba1f24826572a8d21b6abae
PAZDerev <zdereff.pasha@ya.ru>
2024-10-06 12:40:29 +0500

    style () : checkstyle fix

--- project/src/main/java/caselab/controller/document/payload/DocumentResponseDTO.java
+++ project/src/main/java/caselab/controller/document/payload/DocumentResponseDTO.java
@@ -2,9 +2,9 @@ package caselab.controller.document.payload;
 
 import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Schema;
+import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
-import java.util.List;
 
 @Getter
 @Setter
