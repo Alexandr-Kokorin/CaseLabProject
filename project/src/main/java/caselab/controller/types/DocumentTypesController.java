16ba7e4e9acb2e5b903eb96bf2cfbb64d64e9db9
Alexandr-Kokorin <105930009+Alexandr-Kokorin@users.noreply.github.com>
2024-10-06 13:02:18 +0300

    Добавлена авторизация в Swagger

--- project/src/main/java/caselab/controller/types/DocumentTypesController.java
+++ project/src/main/java/caselab/controller/types/DocumentTypesController.java
@@ -4,6 +4,7 @@ import caselab.controller.types.payload.DocumentTypeRequest;
 import caselab.controller.types.payload.DocumentTypeResponse;
 import caselab.service.types.DocumentTypesService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -23,18 +24,21 @@ public class DocumentTypesController {
     private final DocumentTypesService documentTypesService;
 
     @Operation(summary = "Добавить тип документа")
+    @SecurityRequirement(name = "JWT")
     @PostMapping
     public DocumentTypeResponse createDocumentType(@RequestBody DocumentTypeRequest documentTypeRequest) {
         return documentTypesService.createDocumentType(documentTypeRequest);
     }
 
     @Operation(summary = "Получить тип документа по id")
+    @SecurityRequirement(name = "JWT")
     @GetMapping("/{id}")
     public DocumentTypeResponse findDocumentTypeById(@PathVariable Long id) {
         return documentTypesService.findDocumentTypeById(id);
     }
 
     @Operation(summary = "Обновить тип документа")
+    @SecurityRequirement(name = "JWT")
     @PatchMapping("/{id}")
     public DocumentTypeResponse updateDocumentType(@PathVariable Long id,
         @RequestBody DocumentTypeRequest documentTypeRequest) {
@@ -42,6 +46,7 @@ public class DocumentTypesController {
     }
 
     @Operation(summary = "Удалить тип документа по id")
+    @SecurityRequirement(name = "JWT")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteDocumentTypeById(@PathVariable Long id) {
         documentTypesService.deleteDocumentTypeById(id);
