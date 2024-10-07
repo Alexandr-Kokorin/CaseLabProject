eece5270fe424c8817c00be319054e929a9ea93e
Denis Karpov <karpovdenis.al@gmail.com>
2024-10-07 20:35:25 +0300

    Add builder annotation

--- project/src/main/java/caselab/domain/entity/AttributeValue.java
+++ project/src/main/java/caselab/domain/entity/AttributeValue.java
@@ -8,12 +8,14 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.MapsId;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
