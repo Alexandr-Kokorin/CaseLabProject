4bf0a465a01402bb1e858969c10656672d8ddcb4
Denis Karpov <karpovdenis.al@gmail.com>
2024-10-07 19:43:41 +0300

    Uncomment documents field

--- project/src/main/java/caselab/controller/users/payload/UserResponse.java
+++ project/src/main/java/caselab/controller/users/payload/UserResponse.java
@@ -1,12 +1,14 @@
 package caselab.controller.users.payload;
 
+import caselab.controller.document.payload.DocumentResponse;
+import java.util.List;
 import lombok.Builder;
 
 @Builder
 public record UserResponse(
     Long id,
     String login,
-    String displayName
-    //List<DocumentResponse> documents
+    String displayName,
+    List<DocumentResponse> documents
 ) {
 }
