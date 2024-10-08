3cdc050f8faca8d617b640bd634dc59ed53dacb4
Denis Karpov <karpovdenis.al@gmail.com>
2024-10-08 22:03:35 +0300

    Use message.properties

--- project/src/main/java/caselab/controller/users/payload/UserUpdateRequest.java
+++ project/src/main/java/caselab/controller/users/payload/UserUpdateRequest.java
@@ -5,10 +5,10 @@ import lombok.Builder;
 
 @Builder
 public record UserUpdateRequest(
-    @Size(max = 50, message = "Display name must not exceed 50 characters")
+    @Size(max = 50, message = "{user.update.request.display_name.invalid_size}")
     String displayName,
 
-    @Size(min = 6, max = 100, message = "Password must be between 6 and 100 characters")
+    @Size(min = 6, max = 100, message = "{user.update.request.password.invalid_size}")
     String password
 ) {
 }
