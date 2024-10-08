774fa8e33c43fbf6b947de325fe6fcbdbb47550b
Denis Karpov <karpovdenis.al@gmail.com>
2024-10-08 18:57:04 +0300

    Add ResponseEntity to methods and remove response status annotation

--- project/src/main/java/caselab/controller/users/ApplicationUserController.java
+++ project/src/main/java/caselab/controller/users/ApplicationUserController.java
@@ -7,7 +7,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -26,38 +25,41 @@ public class ApplicationUserController {
     private final ApplicationUserService userService;
 
     @GetMapping
-    @ResponseStatus(HttpStatus.OK)
     @Operation(summary = "Получить информацию о всех пользователях",
                description = "Возвращает информацию о каждом пользователе")
-    public List<UserResponse> findAllUsers() {
-        return userService.findAllUsers();
+    public ResponseEntity<List<UserResponse>> findAllUsers() {
+        List<UserResponse> users = userService.findAllUsers();
+        return ResponseEntity.ok(users);
     }
 
     @GetMapping("/{id}")
-    @ResponseStatus(HttpStatus.OK)
     @Operation(summary = "Получить информацию о пользователе",
                description = "Возвращает информацию о пользователе по его идентификатору.")
-    public UserResponse findUserById(@PathVariable Long id) {
-        return userService.findUser(id);
+    public ResponseEntity<UserResponse> findUserById(@PathVariable Long id) {
+        UserResponse user = userService.findUser(id);
+        return ResponseEntity.ok(user);
     }
 
     @PutMapping("/{id}")
-    @ResponseStatus(HttpStatus.OK)
     @PreAuthorize("#id == authentication.principal.id")
     @Operation(summary = "Обновить данные пользователя",
                description = "Обновляет данные пользователя по его идентификатору, "
                    + "если авторизованный пользователь — владелец аккаунта.")
-    public UserResponse updateUser(@PathVariable Long id, @RequestBody UserUpdateRequest updateRequest) {
-        return userService.updateUser(id, updateRequest);
+    public ResponseEntity<UserResponse> updateUser(
+        @PathVariable Long id,
+        @RequestBody UserUpdateRequest updateRequest
+    ) {
+        UserResponse updatedUser = userService.updateUser(id, updateRequest);
+        return ResponseEntity.ok(updatedUser);
     }
 
     @DeleteMapping("/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
     @PreAuthorize("#id == authentication.principal.id")
     @Operation(summary = "Удалить пользователя",
                description = "Удаляет пользователя по идентификатору, "
                    + "если авторизованный пользователь — владелец аккаунта.")
-    public void deleteUser(@PathVariable Long id) {
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
         userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
     }
 }
