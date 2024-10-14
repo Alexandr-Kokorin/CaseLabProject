85d8285c5ff5b4092b559e83dcecf37584897a1d
mudan <myudanov223@gmail.com>
2024-10-14 18:01:44 +0300

    Поправлен checkstyle

--- project/src/main/java/caselab/exception/entity/DocumentVersionNotFoundException.java
+++ project/src/main/java/caselab/exception/entity/DocumentVersionNotFoundException.java
@@ -3,7 +3,7 @@ package caselab.exception.entity;
 import lombok.EqualsAndHashCode;
 
 @EqualsAndHashCode(callSuper = true)
-public class DocumentVersionNotFoundException extends EntityNotFoundException{
+public class DocumentVersionNotFoundException extends EntityNotFoundException {
     public DocumentVersionNotFoundException(Long id) {
         super("document.version.not.found", id);
     }
