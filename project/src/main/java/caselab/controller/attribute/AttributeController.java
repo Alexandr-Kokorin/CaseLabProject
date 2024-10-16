4e767bd957400ec1925b2772a12b171cb8e3cadc
mudan <myudanov223@gmail.com>
2024-10-16 12:51:59 +0300

    Настроен swagger для аттрибутов

--- project/src/main/java/caselab/controller/attribute/AttributeController.java
+++ project/src/main/java/caselab/controller/attribute/AttributeController.java
@@ -4,10 +4,17 @@ import caselab.controller.attribute.payload.AttributeRequest;
 import caselab.controller.attribute.payload.AttributeResponse;
 import caselab.service.attribute.AttributeService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,29 +29,67 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/attributes")
 @SecurityRequirement(name = "JWT")
 @RequiredArgsConstructor
+@Tag(name = "Атрибуты", description = "API взаимодействия с атрибутами типов документов")
 public class AttributeController {
 
     private final AttributeService attributeService;
 
-    @Operation(summary = "Добавить атрибут")
+    @Operation(summary = "Добавить атрибут",
+               description = "Добавляет атрибут в базу данных")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Успешное сохранение",
+                     content = @Content(schema = @Schema(implementation = AttributeResponse.class))),
+        @ApiResponse(responseCode = "400", description = "Неверный ввод",
+                     content = @Content(schema = @Schema(implementation = ProblemDetail.class))),
+        @ApiResponse(responseCode = "403", description = "Ошибка аутентификации",
+                     content = @Content(schema = @Schema(implementation = ProblemDetail.class)))
+    })
     @PostMapping
     public AttributeResponse createAttribute(@Valid @RequestBody AttributeRequest attributeRequest) {
         return attributeService.createAttribute(attributeRequest);
     }
 
-    @Operation(summary = "Получить атрибут по id")
+    @Operation(summary = "Получить атрибут по id",
+               description = "Возвращает атрибут по его id")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Успешное получение",
+                     content = @Content(schema = @Schema(implementation = AttributeResponse.class))),
+        @ApiResponse(responseCode = "404", description = "Атрибут с указанным id не найден",
+                     content = @Content(schema = @Schema(implementation = ProblemDetail.class))),
+        @ApiResponse(responseCode = "403", description = "Ошибка аутентификации",
+                     content = @Content(schema = @Schema(implementation = ProblemDetail.class)))
+    })
     @GetMapping("/{id}")
     public AttributeResponse findAttributeById(@PathVariable Long id) {
         return attributeService.findAttributeById(id);
     }
 
-    @Operation(summary = "Получить список всех атрибутов")
+    @Operation(summary = "Получить список всех атрибутов",
+               description = "Возвращает список всех атрибутов")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Успешное получение",
+                     content = @Content(
+                         array = @ArraySchema(schema = @Schema(implementation = AttributeResponse.class)))),
+        @ApiResponse(responseCode = "403", description = "Ошибка аутентификации",
+                     content = @Content(schema = @Schema(implementation = ProblemDetail.class)))
+    })
     @GetMapping
     public List<AttributeResponse> findAllAttributes() {
         return attributeService.findAllAttributes();
     }
 
-    @Operation(summary = "Изменить атрибут")
+    @Operation(summary = "Обновить атрибут",
+               description = "Обновляет атрибут в базе данных")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Успешное обновление",
+                     content = @Content(schema = @Schema(implementation = AttributeResponse.class))),
+        @ApiResponse(responseCode = "400", description = "Неверный ввод",
+                     content = @Content(schema = @Schema(implementation = ProblemDetail.class))),
+        @ApiResponse(responseCode = "404", description = "Атрибут с указанным id не найден",
+                     content = @Content(schema = @Schema(implementation = ProblemDetail.class))),
+        @ApiResponse(responseCode = "403", description = "Ошибка аутентификации",
+                     content = @Content(schema = @Schema(implementation = ProblemDetail.class)))
+    })
     @PutMapping("/{id}")
     public AttributeResponse updateAttribute(
         @PathVariable Long id,
@@ -53,7 +98,16 @@ public class AttributeController {
         return attributeService.updateAttribute(id, attributeRequest);
     }
 
-    @Operation(summary = "Удалить атрибут")
+    @Operation(summary = "Удаляет атрибут по id",
+               description = "Удаляет атрибут по id из базы данных")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "204", description = "Успешное удаление",
+                     content = @Content),
+        @ApiResponse(responseCode = "404", description = "Атрибут с указанным id не найден",
+                     content = @Content(schema = @Schema(implementation = ProblemDetail.class))),
+        @ApiResponse(responseCode = "403", description = "Ошибка аутентификации",
+                     content = @Content(schema = @Schema(implementation = ProblemDetail.class)))
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteAttribute(@PathVariable Long id) {
         attributeService.deleteAttribute(id);
