a4a9dd50790d17fb62d849f03ce356118158e6ac
TimurTimergalin <timur20041229@gmail.com>
2024-10-26 23:51:03 +0300

    Обновлены статусы документа

--- project/src/main/java/caselab/domain/entity/enums/DocumentStatus.java
+++ project/src/main/java/caselab/domain/entity/enums/DocumentStatus.java
@@ -2,8 +2,11 @@ package caselab.domain.entity.enums;
 
 public enum DocumentStatus {
     DRAFT,
-    IN_PROGRESS,
-    ACCEPTED,
-    REJECTED,
+    SIGNATURE_IN_PROGRESS,
+    VOTING_IN_PROGRESS,
+    SIGNATURE_ACCEPTED,
+    VOTING_ACCEPTED,
+    SIGNATURE_REJECTED,
+    VOTING_REJECTED,
     ARCHIVED
 }
