a70b9cb139e8264b0aeb0e89e739d56b9e5bb6a0
FkishDaniels <irina2808777@gmail.com>
2024-11-17 22:10:50 +0300

    swagger TariffController

--- project/src/main/java/caselab/controller/billing/tariff/TariffController.java
+++ project/src/main/java/caselab/controller/billing/tariff/TariffController.java
@@ -4,12 +4,19 @@ import caselab.controller.billing.tariff.payload.CreateTariffRequest;
 import caselab.controller.billing.tariff.payload.TariffResponse;
 import caselab.controller.billing.tariff.payload.UpdateTariffRequest;
 import caselab.service.billing.tariff.TariffService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -30,12 +37,31 @@ import org.springframework.web.bind.annotation.RestController;
 public class TariffController {
     private final TariffService tariffService;
 
+    @Operation(summary = "Получить тарифф по id",
+               description = "Возвращает тарифф по его id")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Успешное получение",
+                     content = @Content(schema = @Schema(implementation = TariffResponse.class))),
+        @ApiResponse(responseCode = "404", description = "Тарифф с указанным id не найден",
+                     content = @Content(schema = @Schema(implementation = ProblemDetail.class))),
+        @ApiResponse(responseCode = "403", description = "Ошибка аутентификации",
+                     content = @Content(schema = @Schema(implementation = ProblemDetail.class)))
+    })
     @GetMapping("/{id}")
     public TariffResponse getTariffById(@PathVariable("id") Long id) {
         return tariffService.findById(id);
     }
 
-
+    @Operation(summary = "Добавить тарифф",
+               description = "Добавляет тарифф в базу данных")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Успешное сохранение",
+                     content = @Content(schema = @Schema(implementation = TariffResponse.class))),
+        @ApiResponse(responseCode = "400", description = "Неверный ввод",
+                     content = @Content(schema = @Schema(implementation = ProblemDetail.class))),
+        @ApiResponse(responseCode = "403", description = "Ошибка аутентификации",
+                     content = @Content(schema = @Schema(implementation = ProblemDetail.class)))
+    })
     @PostMapping
     public TariffResponse createTariff(
         Authentication authentication,
@@ -43,6 +69,18 @@ public class TariffController {
         return tariffService.createTariff(authentication, tariffRequest);
     }
 
+    @Operation(summary = "Обновить тарифф",
+               description = "Обновляет тарифф в базе данных")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Успешное обновление",
+                     content = @Content(schema = @Schema(implementation = TariffResponse.class))),
+        @ApiResponse(responseCode = "400", description = "Неверный ввод",
+                     content = @Content(schema = @Schema(implementation = ProblemDetail.class))),
+        @ApiResponse(responseCode = "404", description = "Тип документа с указанным id не найден",
+                     content = @Content(schema = @Schema(implementation = ProblemDetail.class))),
+        @ApiResponse(responseCode = "403", description = "Ошибка аутентификации",
+                     content = @Content(schema = @Schema(implementation = ProblemDetail.class)))
+    })
     @PutMapping("/{id}")
     public TariffResponse updateTariff(
         Authentication authentication,
@@ -51,6 +89,16 @@ public class TariffController {
         return tariffService.updateTariff(authentication, id, tariffRequest);
     }
 
+    @Operation(summary = "Удалить тариф по id",
+               description = "Удаляет тариф по id из базы данных")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "204", description = "Успешное удаление",
+                     content = @Content),
+        @ApiResponse(responseCode = "404", description = "Тариф с указанным id не найден",
+                     content = @Content(schema = @Schema(implementation = ProblemDetail.class))),
+        @ApiResponse(responseCode = "403", description = "Ошибка аутентификации",
+                     content = @Content(schema = @Schema(implementation = ProblemDetail.class)))
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<Void>  deleteTariff(
         Authentication authentication,
@@ -59,6 +107,15 @@ public class TariffController {
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(summary = "Получить список всех тарифов",
+               description = "Возвращает список всех тарифов")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Успешное получение",
+                     content = @Content(
+                         array = @ArraySchema(schema = @Schema(implementation = TariffResponse.class)))),
+        @ApiResponse(responseCode = "403", description = "Ошибка аутентификации",
+                     content = @Content(schema = @Schema(implementation = ProblemDetail.class)))
+    })
     @GetMapping
     public Page<TariffResponse> getAllTariffs(
         @RequestParam(value = "pageNum", required = false) Integer pageNum,
