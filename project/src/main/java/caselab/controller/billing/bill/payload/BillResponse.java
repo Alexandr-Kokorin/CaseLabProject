8370ecfdbc8ddb6a743e0ceac31d9fc0bfe9760a
hottabych04 <hottabych04@gmail.com>
2024-11-19 17:25:25 +0300

    Create payload for bills and mapper

---
+++ project/src/main/java/caselab/controller/billing/bill/payload/BillResponse.java
@@ -0,0 +1,20 @@
+package caselab.controller.billing.bill.payload;
+
+import caselab.controller.billing.tariff.payload.TariffResponse;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
+import java.time.LocalDateTime;
+
+@Schema(description = "Ответ, содержащий информациб о биллинге")
+public record BillResponse(
+    @JsonProperty("id")
+    @Schema(description = "Айди биллинга", example = "1")
+    Long id,
+    @JsonProperty("tariff")
+    @Schema(description = "Тариф", example = "1")
+    TariffResponse tariff,
+    @JsonProperty("issued_at")
+    @Schema(description = "Дата оформления")
+    LocalDateTime issuedAt
+) {
+}
