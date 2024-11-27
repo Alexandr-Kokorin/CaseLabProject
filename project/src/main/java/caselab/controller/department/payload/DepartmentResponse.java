d01ae82312ccdae9c375380e964746639c32641a
seshxyz <paratrofer@yandex.ru>
2024-11-27 17:24:58 +0300

    Department structure added

---
+++ project/src/main/java/caselab/controller/department/payload/DepartmentResponse.java
@@ -0,0 +1,49 @@
+package caselab.controller.department.payload;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.OrderBy;
+import java.util.List;
+import lombok.Builder;
+
+@Builder(toBuilder = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public record DepartmentResponse(
+
+    @Schema(description = "ID подразделения", example = "1")
+    @JsonProperty("id")
+    Long id,
+
+    @Schema(description = "Наименование подразделения", example = "Отдел отделов")
+    @JsonProperty("name")
+    String name,
+
+    @Schema(description = "Активно ли подразделение", example = "true")
+    @JsonProperty("is_active")
+    Boolean isActive,
+
+    @Schema(description = "Является ли подразделение верхнеуровневым", example = "false")
+    @JsonProperty("is_top_department")
+    Boolean topDepartment,
+
+    @Schema(description = "Идентификатор родительского подразделения", example = "1")
+    @JsonProperty("parent_department_id")
+    Long parentDepartment,
+
+    @Schema(description = "ФИО и электронная почта руководителя", example = "Иванов Иван Иванович [user@example.com")
+    @JsonProperty("head_of_department")
+    String headOfDepartment,
+
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
+    @JsonProperty("employees")
+    @OrderBy("name ASC")
+    List<EmployeeResponse> employees,
+
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
+    @JsonProperty("child_departments")
+    @OrderBy("id ASC")
+    List<DepartmentResponse> childDepartments
+
+) {
+}
---
+++ project/src/main/java/caselab/exception/entity/already_exists/DepartmentAlreadyExistsException.java
@@ -0,0 +1,11 @@
+package caselab.exception.entity.already_exists;
+
+import caselab.exception.base.ApplicationConflictException;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(callSuper = true)
+public final class DepartmentAlreadyExistsException extends ApplicationConflictException {
+    public DepartmentAlreadyExistsException(String name, Long id) {
+        super("department.with.this.name.and.parent.already_exists", new Object[]{name,id});
+    }
+}
