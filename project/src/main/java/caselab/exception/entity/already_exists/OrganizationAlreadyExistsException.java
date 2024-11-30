104d71b03b4221daeccee59eb1dd1d82fd3ba5ef
Denis Karpov <karpovdenis.al@gmail.com>
2024-11-30 21:40:50 +0300

    Add organization exception

---
+++ project/src/main/java/caselab/exception/entity/already_exists/OrganizationAlreadyExistsException.java
@@ -0,0 +1,11 @@
+package caselab.exception.entity.already_exists;
+
+import caselab.exception.base.ApplicationConflictException;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(callSuper = true)
+public class OrganizationAlreadyExistsException extends ApplicationConflictException {
+    public OrganizationAlreadyExistsException(String id) {
+        super("organization.already_exists", new Object[]{id});
+    }
+}
